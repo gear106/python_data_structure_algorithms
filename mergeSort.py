9e354493e851409d8b0c85df1890578d38d7c4d7
libinglin <binglinli106@126.com>
2018-08-26 18:24:29 +0800

    added mergeSort

---
+++ mergeSort.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug 26 18:08:48 2018
+
+@author: GEAR
+"""
+
+'''
+归并排序
+'''
+
+def mergeSort(alist):
+    print('splitting', alist)
+    if len(alist) > 1:
+        mid = len(alist) // 2
+        lefthalf = alist[:mid]
+        righthalf = alist[mid:]
+        
+        mergeSort(lefthalf)
+        mergeSort(righthalf)
+        
+        i, j, k = 0, 0 ,0 
+        while i < len(lefthalf) and j < len(righthalf):
+            if lefthalf[i] < righthalf[j]:
+                alist[k] = lefthalf[i]
+                i += 1
+            else:
+                alist[k] = righthalf[j]
+                j += 1
+            k += 1
+            
+        while i < len(lefthalf):
+            alist[k] = lefthalf[i]
+            i += 1
+            k += 1
+        while j < len(righthalf):
+            alist[k] = righthalf[j]
+            j += 1
+            k += 1
+        print('Merging', alist)
+
+
+if __name__ == '__main__':
+    test = [54,26,93,17,77,31,44,55,20]
+    mergeSort(test)
+    print(test)
+        
