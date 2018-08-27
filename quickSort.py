7acdb2c7bbc2dd587df3a5503b7f8cf81323d4f5
libinglin <binglinli106@126.com>
2018-08-27 21:47:29 +0800

    modified quickSort

--- quickSort.py
+++ quickSort.py
@@ -4,6 +4,12 @@ Created on Mon Aug 27 20:17:51 2018
 
 @author: GEAR
 """
+'''
+快速排序算法：
+1.先从数列数取出一个数作为基准数（枢轴值）
+2.分区过程，将比这个数大的数全部放到它右边， 小于或等于它的数全部放到它左边
+3.在对左右区间重复第二部，直到各区间只有一个数
+'''
 
 def quickSort1(alist):
     quickSortHelpter(alist, 0 , len(alist)-1)
@@ -11,11 +17,14 @@ def quickSort1(alist):
 def quickSortHelpter(alist, first, last):
     if first < last:
         splitpoint = partition(alist, first, last)
-        quickSortHelpter(alist, first, splitpoint-1)
-        quickSortHelpter(alist, splitpoint+1, last)
+        quickSortHelpter(alist, first, splitpoint-1) #左边排序
+        quickSortHelpter(alist, splitpoint+1, last)  #右边排序
         
 
 def partition(alist, first, last):
+    '''
+    将选取的基准数移动到排序正确的位置,返回值为其正确位置的索引
+    '''
     pivotvalue = alist[first]
     leftmark = first + 1
     rightmark = last
@@ -66,8 +75,6 @@ def quickSort2(alist, first, last):
     quickSort2(alist, leftmark+1, last)
     
     
-
-
 if __name__ == '__main__':
     test = [54,26,93,17,77,31,44,55,20]
     quickSort2(test, 0 , len(test)-1)
