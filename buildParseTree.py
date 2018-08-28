73861bfc2b0f9d94bcf34bbf44d11a9ce1c82e4f
libinglin <binglinli106@126.com>
2018-08-28 19:30:07 +0800

    modified buildParseTree

--- buildParseTree.py
+++ buildParseTree.py
@@ -7,10 +7,50 @@ Created on Tue Aug 28 18:02:43 2018
 
 from Stack import *
 from NodeBinaryTree import *
+import operator
 
 def buildParseTree(fpexp):
-    fplist = fpexp.split()  #这里表达书中间要加空格
+    fplist = fpexp.split()  #这里表达式中间要加空格
     pStack = Stack()
     eTree = BinaryTree('')
     pStack.push(eTree)
+    currentTree = eTree
+    
+    for i in fplist:
+        if i == '(':
+            currentTree.insertLeft('')
+            pStack.push(currentTree)
+            currentTree = currentTree.getLeftChild()
+        elif i not in ['+', '-', '*', '/', ')']:
+            currentTree.setRootVal(int(i))
+            parent = pStack.pop()
+            currentTree = parent
+        elif i in ['+', '-', '*', '/']:
+            currentTree.setRootVal(i)
+            currentTree.insertRight('')
+            pStack.push(currentTree)
+            currentTree = currentTree.getRightChild()
+        elif i == ')':
+            currentTree = pStack.pop()
+        else:
+            raise ValueError
+    return eTree
+
+def evaluate(parseTree):
+    opers = {'+':operator.add, '-':operator.sub, '*':operator.mul, '/':operator.truediv}
+    
+    leftC = parseTree.getLeftChild()
+    rightC = parseTree.getRightChild()
+    
+    if leftC and rightC:
+        fn = opers[parseTree.getRootVal()]
+        return fn(evaluate(leftC), evaluate(rightC))
+    else:
+        return parseTree.getRootVal()
+    
+    
+
+
+pt = buildParseTree('( ( 10 + 5 ) * 3 )')
+print(evaluate(pt))  # 正确输出结果应为45
     
